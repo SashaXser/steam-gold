597ac7747533ce47391ea1ff6a2212c398156377
FUNLABGAMES <funlabgames@gmail.com>
2017-01-02 16:34:03 +0200

    Create steam-gold.js

---
+++ steam-gold.js
@@ -0,0 +1,22 @@
+const SteamUser = require('steam-user');
+const client = new SteamUser();
+
+SteamUser.prototype.setPersona = function(state, name) {
+	this._send(SteamUser.EMsg.ClientChangeStatus, {
+		"persona_state": state,
+		"persona_state_flags": 4,
+		"player_name": name
+	});
+};
+
+const logOnOptions = {
+    accountName: process.argv[2],
+    password: process.argv[3]
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+    client.setPersona(SteamUser.Steam.EPersonaState.Online);
+    console.log("Заработало! YOUTUBE.COM/СИРОЖА Нажми CTRL + C Что бы выключить статус.");
+});
